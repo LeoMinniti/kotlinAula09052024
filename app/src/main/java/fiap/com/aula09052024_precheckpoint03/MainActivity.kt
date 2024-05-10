5328daa854534c257a2df4f4023204f3d2ef43e7
Leonardo Minniti <leonsenai@gmail.com>
2024-05-09 22:47:39 -0300

    4.Refatoração da ActivityMain para implementar a funcionalidade de in

--- app/src/main/java/fiap/com/aula09052024_precheckpoint03/MainActivity.kt
+++ app/src/main/java/fiap/com/aula09052024_precheckpoint03/MainActivity.kt
@@ -1,6 +1,8 @@
 package fiap.com.aula09052024_precheckpoint03
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,10 +12,30 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.recyclerview.widget.RecyclerView
+import fiap.com.aula09052024_precheckpoint03.adapter.ItensAdapter
+import fiap.com.aula09052024_precheckpoint03.model.ItemModel
 import fiap.com.aula09052024_precheckpoint03.ui.theme.Aula09052024precheckpoint03Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView);
+        val itemsAdapter = ItensAdapter()
+        recyclerView.adapter = itemsAdapter
+
+        val button = findViewById<Button>(R.id.button)
+        val editText = findViewById<EditText>(R.id.editText)
+
+        button.setOnClickListener {
+            val item = ItemModel(
+                name = editText.text.toString()
+            )
+
+            itemsAdapter.addItem(item)
+        }
     }
 }
