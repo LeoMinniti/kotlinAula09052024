9cdc56a32a9e32ac0bb1809b396beccb39c70157
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-09 22:11:52 -0300

    criando o projeto

---
+++ app/src/main/java/fiap/com/aula09052024_precheckpoint03/MainActivity.kt
@@ -0,0 +1,19 @@
+package fiap.com.aula09052024_precheckpoint03
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import fiap.com.aula09052024_precheckpoint03.ui.theme.Aula09052024precheckpoint03Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
